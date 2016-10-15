15dcaa029973ad98ffb416d1c995e2007fe11bd2
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2016-10-15 01:01:21 -0400

    Added django_debug_toolbar (working now)

--- judge/settings.py
+++ judge/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'judge.urls'
@@ -144,3 +146,5 @@ DEFAULT_FROM_EMAIL = 'l.castillo@lab.matcom.uh.cu'
 # Resources used while grading
 SANDBOX_FOLDER = '/home/leandro/Desktop/@PROJECTS/sandbox'
 PROBLEMS_FOLDER = '/home/leandro/Desktop/@PROJECTS/problems'
+
+INTERNAL_IPS = ['127.0.0.1']
