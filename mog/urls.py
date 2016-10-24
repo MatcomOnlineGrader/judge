df322d1ae06e2e9aa4077a3cdb9bdd9af2635852
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2016-10-24 16:05:20 -0400

    Minor changes

--- mog/urls.py
+++ mog/urls.py
@@ -55,6 +55,4 @@ urlpatterns = [
     url(r'^login/$', views.Login.as_view(), name='login'),
     url(r'^logout/$', views.Logout.as_view(), name='logout'),
     url(r'^register/$', views.Register.as_view(), name='register'),
-
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
