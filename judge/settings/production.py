e2b46deb30bc190170dd6f4ee84c0281d2bd99d5
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2017-02-15 19:01:50 -0500

    Changed email configuration pointing to gmail

--- judge/settings/production.py
+++ judge/settings/production.py
@@ -23,11 +23,11 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 25
-EMAIL_HOST = 'mail.matcom.uh.cu'
-EMAIL_HOST_USER = get_secret_value('production', 'EMAIL_USER')
-EMAIL_HOST_PASSWORD = get_secret_value('production', 'EMAIL_PASS')
-DEFAULT_FROM_EMAIL = 'mog@matcom.uh.cu'
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = get_secret_value('production', 'EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = get_secret_value('production', 'EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = get_secret_value('production', 'DEFAULT_FROM_EMAIL')
 
 # Place to store problem test-cases ( secret location )
 PROBLEMS_FOLDER = get_secret_value('production', 'PROBLEMS_FOLDER')
