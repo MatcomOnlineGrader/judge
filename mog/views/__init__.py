728822178e806e60ae446a17ed19c659b2640d6c
jj <jjavierdguezas@gmail.com>
2017-04-07 20:29:14 -0400

    Added get_csv to contest view

--- mog/views/__init__.py
+++ mog/views/__init__.py
@@ -1,10 +1,10 @@
 from .account import Register, Login, Logout
-from .contest import contests, contest_problems, contest_standing, contest_submissions,\
-    remove_contest, contest_register, contest_remove_instance, contest_remove_registration,\
-    ContestCreateView, ContestEditView, rate_contest, unrate_contest, contest_registration,\
-    contest_register_team, contest_register_user, contest_json
+from .contest import contests, contest_problems, contest_standing, contest_submissions, \
+    remove_contest, contest_register, contest_remove_instance, contest_remove_registration, \
+    ContestCreateView, ContestEditView, rate_contest, unrate_contest, contest_registration, \
+    contest_register_team, contest_register_user, contest_json, contest_csv
 from .post import PostListView, PostDetailView, PostCreateView, EditPostView
-from .problem import remove_problem, problem, ProblemCreateView, ProblemEditView,\
+from .problem import remove_problem, problem, ProblemCreateView, ProblemEditView, \
     ProblemListView, ProblemTestsView, remove_test, TestEditView
 from .submission import submissions, submission, Submit, rejudge
 from .team import create_team, remove_team
