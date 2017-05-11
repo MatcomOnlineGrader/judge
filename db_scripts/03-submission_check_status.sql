38ffcb46dce2ead7be1afed4996002a95abaa4c3
Daniel Otero Baguer <gute1991@gmail.com>
2017-05-11 15:35:12 +0200

    Load mathjax just on pages that need it. Fixing some DB scripts

--- db_scripts/03-submission_check_status.sql
+++ db_scripts/03-submission_check_status.sql
@@ -1,7 +1,10 @@
+-- Trigger: check_update
+
+-- DROP TRIGGER check_update ON public.api_submission;
+
 CREATE TRIGGER check_update
     BEFORE UPDATE OF result_id
     ON public.api_submission
     FOR EACH ROW
     WHEN ((old.result_id IS DISTINCT FROM new.result_id))
-    EXECUTE PROCEDURE public.updatesubmission(problem_id);
-END
+    EXECUTE PROCEDURE public.updatesubmission();
