eb2d1bafa139eb36b3cc4a61e6daeaede90184a6
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2017-05-12 14:35:17 -0400

    add search and filter to UserProfile model in admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from .models import *
 
+
+class UserProfileAdmin(admin.ModelAdmin):
+    class RoleFilter(admin.SimpleListFilter):
+        title = 'role'
+        parameter_name = 'role'
+
+        def lookups(self, request, model_admin):
+            return (
+                ('admin', 'Administrator'),
+                ('browser', 'Code Browser'),
+                ('admin|browser', 'Administrator or Code Browser')
+            )
+
+        def queryset(self, request, queryset):
+            if self.value() == 'admin':
+                return queryset.filter(role='admin')
+            elif self.value() == 'browser':
+                return queryset.filter(role='browser')
+            elif self.value() == 'admin|browser':
+                return queryset.filter(Q(role='admin') | Q(role='browser'))
+
+    search_fields = ('user__username', )
+    list_filter = (RoleFilter, )
+
 admin.site.register(Team)
 admin.site.register(Tag)
 admin.site.register(Checker)
@@ -12,7 +36,7 @@ admin.site.register(Division)
 admin.site.register(Institution)
 admin.site.register(RatingChange)
 admin.site.register(Compiler)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Result)
 admin.site.register(Submission)
 admin.site.register(Comment)
