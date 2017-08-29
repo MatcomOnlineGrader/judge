96c732fd3504312ea7cdc0c6a995eadd02c0672d
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2017-08-29 04:42:47 -0400

    Fix issues #2 and #15 on problem/user points
    Modify update_submission.sql procedure to change points to affected
    users once a new submission is done. Add 'points' field to UserProfile
    model to store the points the user has. Create a procedure that updates
    the points for all users called update_all_user_points.sql (script).
    The right order is to update problem points and then user points.

--- db_scripts/02-update_submission.sql
+++ db_scripts/02-update_submission.sql
@@ -4,10 +4,20 @@ CREATE OR REPLACE FUNCTION public.updatesubmission()
     COST 100.0
     VOLATILE NOT LEAKPROOF 
 AS $BODY$
-
+DECLARE
+  row record;
+  old_value INTEGER ;
+  new_value INTEGER ;
 BEGIN
   IF NEW.result_id IS DISTINCT FROM OLD.result_id THEN
-    UPDATE api_problem SET points = (SELECT ComputeProblemPoints(NEW.problem_id)) WHERE api_problem.id = NEW.problem_id;
+    SELECT points INTO old_value FROM api_problem WHERE NEW.problem_id = id;
+    SELECT ComputeProblemPoints(NEW.problem_id) INTO new_value FROM api_problem WHERE NEW.problem_id = id;
+
+    FOR row IN (SELECT DISTINCT (user_id) FROM api_submission WHERE (hidden = FALSE) AND (problem_id = NEW.problem_id) AND (result_id = 1)) LOOP
+      UPDATE api_userprofile SET points = (api_userprofile.points - old_value + new_value) WHERE user_id = row.user_id;
+    END LOOP;
+
+    UPDATE api_problem SET points = new_value WHERE api_problem.id = NEW.problem_id;
   END IF;
   RETURN NEW;
 END;
