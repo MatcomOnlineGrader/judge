b39ffc2a76beb3cead20e089df319a030ea979c9
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2018-01-04 01:04:50 -0500

    decode output/error to utf8 in get_exitcode_stdout_stderr

--- api/management/commands/__utils.py
+++ api/management/commands/__utils.py
@@ -7,4 +7,4 @@ def get_exitcode_stdout_stderr(cmd, cwd):
     proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
     out, err = proc.communicate()
     exitcode = proc.returncode
-    return exitcode, out, err
+    return exitcode, out.decode('utf-8'), err.decode('utf-8')
