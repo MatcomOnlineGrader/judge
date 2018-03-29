cb0f2a696aee986b5816f6c39e41485ba7ec72d1
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2018-03-29 02:45:01 -0400

    modifications to grader command
    - execute runexe.exe without --no-idleness-check flag.
    - update FAQ to include "Idleness Limit Exceeded".
    - add python arguments during runexe.exe execution.
    - add a --number_of_executions parameter to grader command to execute
      a test case multiple times (up to number_of_executions - default 2)
      while the run exceeds the time limit.
    - force TLE whenever `passed_time` exceeds the problem time limit.

--- api/management/commands/grader.py
+++ api/management/commands/grader.py
@@ -125,7 +125,7 @@ def report_progress(submission, current_test, number_of_tests, result, bar_width
         sys.stderr.write('\r')
 
 
-def grade_submission(submission):
+def grade_submission(submission, number_of_executions):
     submission.result = Result.objects.get(name__iexact='running')
     submission.save()
 
@@ -143,13 +143,13 @@ def grade_submission(submission):
         return
 
     if compiler.language.lower() == 'java':
-        cmd = '"%s" -t %ds -m %dM -xml -i "{input-file}" -o "{output-file}" --no-idleness-check java -Xms32M -Xmx256M -DMOG=true Main'\
+        cmd = '"%s" -t %ds -m %dM -xml -i "{input-file}" -o "{output-file}" java -Xms32M -Xmx256M -DMOG=true Main'\
               % (RUNEXE_PATH, problem.time_limit, problem.memory_limit)
     elif compiler.language.lower() == 'python':
-        cmd = '"%s" -t %ds -m %dM -xml -i "{input-file}" -o "{output-file}" --no-idleness-check "%s" %s' \
-              % (RUNEXE_PATH, problem.time_limit, problem.memory_limit, compiler.path, '%d.%s' % (submission.id, compiler.file_extension))
+        cmd = '"%s" -t %ds -m %dM -xml -i "{input-file}" -o "{output-file}" "%s" %s %s' \
+              % (RUNEXE_PATH, problem.time_limit, problem.memory_limit, compiler.path, compiler.arguments, '%d.%s' % (submission.id, compiler.file_extension))
     else:
-        cmd = '"%s" -t %ds -m %dM -xml -i "{input-file}" -o "{output-file}" --no-idleness-check %s' \
+        cmd = '"%s" -t %ds -m %dM -xml -i "{input-file}" -o "{output-file}" %s' \
               % (RUNEXE_PATH, problem.time_limit, problem.memory_limit, '%d.%s' % (submission.id, compiler.exec_extension))
 
     i_folder = os.path.join(settings.PROBLEMS_FOLDER, str(problem.id), 'inputs')
@@ -167,53 +167,62 @@ def grade_submission(submission):
 
     for input_file, answer_file in zip(i_files, o_files):
         try:
-            _, out, err = get_exitcode_stdout_stderr(
-                cmd=cmd.format(**{'input-file': input_file, 'output-file': 'output.txt'}),
-                cwd=submission_folder
-            )
-
             # parse runexe output
             def get_tag_value(xml, tag_name):
                 element = xml.getElementsByTagName(tag_name)[0].firstChild
                 return element.nodeValue if element else None
 
-            xml = minidom.parseString(out.strip())
-            invocation_verdict = get_tag_value(xml, 'invocationVerdict')
-            exit_code = int(get_tag_value(xml, 'exitCode'))
-            processor_user_mode_time = int(get_tag_value(xml, 'processorUserModeTime'))
-            processor_kernel_mode_time = int(get_tag_value(xml, 'processorKernelModeTime'))
-            passed_time = int(get_tag_value(xml, 'passedTime'))
-            consumed_memory = int(get_tag_value(xml, 'consumedMemory'))
-            comment = get_tag_value(xml, 'comment') or '<blank>'
-
             current_test += 1
-            maximum_execution_time = max(maximum_execution_time, passed_time)
-            maximum_consumed_memory = max(maximum_consumed_memory, consumed_memory)
-            judgement_details += 'Case#%d [%d bytes][%d ms]: ' % (current_test, consumed_memory, passed_time)
-
-            if invocation_verdict != 'SUCCESS':
-                judgement_details += comment + '\n'
-                result = {
-                    'SECURITY_VIOLATION': 'runtime error',
-                    'MEMORY_LIMIT_EXCEEDED': 'memory limit exceeded',
-                    'TIME_LIMIT_EXCEEDED': 'time limit exceeded',
-                    'IDLENESS_LIMIT_EXCEEDED': 'time limit exceeded',
-                    'CRASH': 'internal error',
-                    'FAIL': 'internal error'
-                }[invocation_verdict]
-            elif exit_code != 0:
-                judgement_details += 'runtime error\n'
-                result = 'runtime error'
-            else:
-                rc, out, err = get_exitcode_stdout_stderr(
-                    cmd=checker_command % (input_file, 'output.txt', answer_file),
+            for _ in range(number_of_executions):
+                _, out, err = get_exitcode_stdout_stderr(
+                    cmd=cmd.format(**{'input-file': input_file, 'output-file': 'output.txt'}),
                     cwd=submission_folder
                 )
-                out = out.strip()
-                err = err.strip()
-                judgement_details += (out or err) + '\n'
-                if rc != 0:
-                    result = 'wrong answer'
+
+                xml = minidom.parseString(out.strip())
+                invocation_verdict = get_tag_value(xml, 'invocationVerdict')
+                exit_code = int(get_tag_value(xml, 'exitCode'))
+                processor_user_mode_time = int(get_tag_value(xml, 'processorUserModeTime'))
+                processor_kernel_mode_time = int(get_tag_value(xml, 'processorKernelModeTime'))
+                passed_time = int(get_tag_value(xml, 'passedTime'))
+                consumed_memory = int(get_tag_value(xml, 'consumedMemory'))
+                comment = get_tag_value(xml, 'comment') or '<blank>'
+
+                if passed_time > problem.time_limit * 1000 and\
+                        (invocation_verdict != 'IDLENESS_LIMIT_EXCEEDED'):
+                    # force TLE when passed time exceed the problem time limit.
+                    invocation_verdict = 'TIME_LIMIT_EXCEEDED'
+
+                if invocation_verdict != 'SUCCESS':
+                    result = {
+                        'SECURITY_VIOLATION': 'runtime error',
+                        'MEMORY_LIMIT_EXCEEDED': 'memory limit exceeded',
+                        'TIME_LIMIT_EXCEEDED': 'time limit exceeded',
+                        'IDLENESS_LIMIT_EXCEEDED': 'idleness limit exceeded',
+                        'CRASH': 'internal error',
+                        'FAIL': 'internal error'
+                    }[invocation_verdict]
+                elif exit_code != 0:
+                    comment = 'runtime error'
+                    result = 'runtime error'
+                else:
+                    rc, out, err = get_exitcode_stdout_stderr(
+                        cmd=checker_command % (input_file, 'output.txt', answer_file),
+                        cwd=submission_folder
+                    )
+                    out = out.strip()
+                    err = err.strip()
+                    comment = (out or err)
+                    if rc != 0:
+                        result = 'wrong answer'
+                if result != 'memory limit exceeded':
+                    # keep running the same test while TLE
+                    break
+            maximum_execution_time = max(maximum_execution_time, passed_time)
+            maximum_consumed_memory = max(maximum_consumed_memory, consumed_memory)
+            judgement_details += 'Case#%d [%d bytes][%d ms]: %s\n' % (
+                current_test, consumed_memory, passed_time, comment
+            )
         except:
             result = 'internal error'
         report_progress(
@@ -241,11 +250,14 @@ class Command(BaseCommand):
         parser.add_argument('--sleep', type=int, default='5', help='Number of seconds to sleep between grade submissions.')
         parser.add_argument('--number_of_graders', type=int, default='1', help='Number of graders running concurrently.')
         parser.add_argument('--grader_index', type=int, default='0', help='Grader index in the range [0, number_of_graders).')
+        parser.add_argument('--number_of_executions', type=int, default='2',
+                            help='Number of executions to prevent TLE')
 
     def handle(self, *args, **options):
         sleep, number_of_graders, grader_index = options.get('sleep'),\
                                                  options.get('number_of_graders'),\
                                                  options.get('grader_index')
+        number_of_executions = options.get('number_of_executions')
         # validate input
         if sleep <= 0:
             raise CommandError('sleep argument must to be positive')
@@ -253,6 +265,8 @@ class Command(BaseCommand):
             raise CommandError('number_of_graders argument must to be positive')
         if not (0 <= grader_index < number_of_graders):
             raise CommandError('grader_index argument must to be in the range [0, %d)' % number_of_graders)
+        if number_of_executions < 1:
+            raise CommandError('number_of_executions must to be a positive integer')
         # store compilers
         colorama.init()
         while True:
@@ -262,7 +276,7 @@ class Command(BaseCommand):
                     create_submission_folder(submission)
                     if check_problem_folder(submission.problem):
                         if compile_submission(submission):
-                            grade_submission(submission)
+                            grade_submission(submission, number_of_executions)
                     else:
                         set_internal_error(submission, 'internal error, problem not ready')
                     remove_submission_folder(submission)
