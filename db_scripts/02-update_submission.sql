d0a2929f6c713c8c65b7e05349b6e9c6209325dc
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2018-04-06 15:19:36 -0400

    add frozen/death time feature
    In order to allow frozen/death time in standings, few changes were
    needed. First, a field called `status` was added to Submission model;
    `status` can be "normal", "frozen" or "death". Values "frozen" and
    "death" are reserved only for submissions sent in the real contest in
    frozen and death intervals respectively. The value for `status` is
    assigned when the user submits his code.
    
    The most difficult part of frozen/death feature is trying to hide
    results from the user. That is, avoid the user can know whether his
    submissions was accepted or not. The user can find out doing the
    following "tricks":
    
    Points
    - Checking the number of `points` he/she has. If the number of points
      increases, then the submission was accepted. To prevent this, stored
      procedures were modified to take `status` column into consideration.
      Scripts in directory db_scripts/ will consider accepted-normal-visible
      submissions only to calculate the number of points for the user and
      for the problem. Thus `status` values with "frozen" or "death" will
      be ignored.
    - The user can look for the number of points a problem has. This is
      fixed at db_scripts/01-compute_problem_score.sql
    
    User profile
    - Checking the number of `accepted` submissions he/she has. This value
      doesn't depend on stored procedures, instead, it is calculated at
      UserProfile.accepted_submissions property.
    - Closely related to the previous value, the user can look for the
      number of accepted problems calculated at UserProfile.solved_problems
    
    Problem
    - Checking the number of accepted solutions a problem has in the contest
      problems view. This is calculated at Problem.solved_in_real which
      depends on Problem._accepted_submissions
    - Checking the number of accepted solutions a problem has in the problem
      list view. This is calculated at Problem.solved which depends on
      Problem._accepted_submissions
    
    Filter
    - The user can try to get some info filtering in submission list view.
      This is handled at judge.mog.helpers.filter_submissions function. If
      "pending" result is specified, then it will list pending submissions
      along with those labeled with "frozen" and "death".
    
    Security
    - The system must hide frozen/death submissions according to the user
      who is requesting the information. This happens in three places
      combined with four type of users.
      * places: standing, submission list (/submissions/), submission
                details (/submission/<id>)
      * users: anonymous, logged and not registered, logged and registered,
               administrator.
    
    ------------------------------------------------------------------------
    
    Other changes in this commit:
    - added a migration that will execute SQL scripts located at db_scripts/
    - modify README.md to include additional commands that must be executed
      in postgres.
    - added some translation (locale/es/LC_MESSAGES/django.po)

--- db_scripts/02-update_submission.sql
+++ db_scripts/02-update_submission.sql
@@ -9,12 +9,11 @@ AS $BODY$
 
 DECLARE
   row RECORD;
-  user_points INTEGER;
 BEGIN
   -- update problem points
   UPDATE api_problem SET points = (SELECT compute_problem_points(api_problem.id)) WHERE api_problem.id = NEW.problem_id;
   -- update user points
-  FOR row IN (SELECT DISTINCT (user_id) FROM api_submission WHERE (hidden = FALSE) AND (problem_id = NEW.problem_id)) LOOP
+  FOR row IN (SELECT DISTINCT (user_id) FROM api_submission WHERE problem_id = NEW.problem_id) LOOP
     UPDATE api_userprofile SET points = (SELECT compute_user_points(row.user_id)) WHERE api_userprofile.user_id = row.user_id;
   END LOOP;
   RETURN NEW;
