59f11328be837a206e80efde9043cd0312a52650
Marcelo Fornet <mfornet94@gmail.com>
2019-09-04 03:20:26 +0000

    New logic for calculating standing
    Rework calculate standing to make instead of `n * m` queries to the database (one per pair participant x problem) do 3 queries where retrieved information is only the necessary.
    
    We request relevant problems, participants and submissions from the datatable (3 queries) and then populate the standing with proper values, taking care of user visibility (different users might have different views of the same standing).
    
    Discussion in !15

--- mog/utils.py
+++ mog/utils.py
@@ -3,7 +3,6 @@ import os
 
 from bs4 import BeautifulSoup
 from django.conf import settings
-from django.db.models import Q
 from django.utils.safestring import mark_safe
 
 
@@ -71,6 +70,11 @@ def user_is_observer(user):
     return user.is_authenticated and hasattr(user, 'profile') and user.profile.is_observer
 
 
+def user_can_bypass_frozen(user):
+    """return True iff logged user can see submissions and standing in frozen/death time"""
+    return user_is_admin(user) or user_is_browser(user) or user_is_observer(user)
+
+
 def user_rating(user):
     """return user rating"""
     return user.profile.rating if hasattr(user, 'profile') else 0
@@ -126,167 +130,6 @@ def write_to_test(problem, folder, test, content):
         path = os.path.join(settings.PROBLEMS_FOLDER, str(problem.id), folder, test)
         with open(path, 'w') as f:
             f.write(content)
-
-
-class ProblemResult(object):
-    def __init__(self, accepted=False, attempts=False, acc_delta=None, first=False, first_all=False, pending=0):
-        self.accepted = accepted
-        self.attempts = attempts
-        self.acc_delta = acc_delta
-        self.first = first
-        self.first_all = first_all
-        self.pending = pending
-
-    def delta(self):
-        if self.acc_delta:
-            return self.acc_delta.total_seconds()
-        return float('inf')
-
-
-class InstanceResult(object):
-    def __init__(self, instance=None, problem_results=None, rank=1, solved=0, penalty=0):
-        self.instance = instance
-        self.solved = solved
-        self.rank = rank
-        self.penalty = penalty
-        self.problem_results = []
-        if problem_results:
-            for problem_result in problem_results:
-                self.add_problem_result(problem_result)
-
-    def attempts(self):
-        return sum([pr.attempts for pr in self.problem_results])
-
-    def add_problem_result(self, problem_result):
-        if problem_result.accepted:
-            self.solved += 1
-            self.penalty += problem_result.acc_delta.total_seconds() / 60 + 20 * problem_result.attempts
-        self.problem_results.append(problem_result)
-
-
-def calculate_standing(contest, virtual=False, user_instance=None, group=None, admin=False):
-    instances = contest.instances.all() if virtual else \
-        contest.instances.filter(real=True)
-    if group:
-        instances = instances.filter(group=group)
-    if user_instance:
-        user_instance_relative_time =\
-            user_instance.relative_time
-    problems = contest.problems.order_by('position')
-    instance_results = []
-    for instance in instances:
-        instance_result = InstanceResult(instance=instance)
-        for problem in problems:
-            if admin:
-                # user is administrator, this imply he/she can see the contest
-                # without any pending submissions.
-                pending_submissions = 0
-                submissions = instance.submissions\
-                    .filter(Q(problem=problem) & Q(hidden=False))
-            elif user_instance:
-                if user_instance.real:
-                    # if the instance looking at the ranking is real (participant),
-                    # then two cases may follow:
-                    # 1) The user is looking at his/her row. In this case he/she will
-                    #    be able to see all his/her submissions except death
-                    #    submissions (frozen & normal). Pending submissions will
-                    #    be death submissions.
-                    # 2) The user is looking at another row. In this case he/she will
-                    #    be able to see only normal submissions for this row. Pending
-                    #    submissions will be frozen & death submissions.
-                    if user_instance == instance:
-                        # case1
-                        pending_submissions = instance.submissions\
-                            .filter(Q(problem=problem) & Q(hidden=False) & Q(status='death')).count()
-                        submissions = instance.submissions\
-                            .filter(Q(problem=problem) & Q(hidden=False) & (Q(status='normal') | Q(status='frozen')))
-                    else:
-                        # case2
-                        pending_submissions = instance.submissions\
-                            .filter(Q(problem=problem) & Q(hidden=False) & ~Q(status='normal')).count()
-                        submissions = instance.submissions\
-                            .filter(Q(problem=problem) & Q(hidden=False) & Q(status='normal'))
-                else:
-                    # if the instance is virtual, then it will simulate the contest,
-                    # the number of pending submissions will be 0 because here frozen/death
-                    # time are disabled.
-                    pending_submissions = 0
-                    if instance.real:
-                        submissions = instance.submissions.filter(
-                            Q(problem=problem) &
-                            Q(hidden=False) &
-                            Q(date__lte=(contest.start_date + user_instance_relative_time))
-                        )
-                    else:
-                        submissions = instance.submissions.filter(
-                            Q(problem=problem) &
-                            Q(hidden=False) &
-                            Q(date__lte=(instance.start_date + user_instance_relative_time))
-                        )
-            else:
-                # user not registered in contest, in this case, only normal solutions will
-                # be displayed (solutions before frozen time), pending solutions will be
-                # those submissions marked as 'frozen' or 'death'.
-                submissions = instance.submissions\
-                    .filter(Q(problem=problem) & Q(hidden=False) & Q(status='normal'))
-                pending_submissions = instance.submissions\
-                    .filter(Q(problem=problem) & Q(hidden=False) & (Q(status='frozen') | Q(status='death'))).count()
-
-            accepted, attempts, acc_delta = 0, False, None
-            accepted_submission = submissions\
-                .filter(result__name__iexact='accepted').order_by('date').first()
-            if accepted_submission:
-                attempts = submissions\
-                    .filter(result__penalty=True, date__lt=accepted_submission.date).count()
-                if instance.real:
-                    acc_delta = accepted_submission.date - contest.start_date
-                else:
-                    acc_delta = accepted_submission.date - instance.start_date
-            else:
-                attempts = submissions.filter(result__penalty=True).count()
-            instance_result.add_problem_result(
-                ProblemResult(accepted=acc_delta, attempts=attempts, acc_delta=acc_delta, pending=pending_submissions)
-            )
-        instance_results.append(instance_result)
-
-    def instance_key(ir):
-        """Used to sort instances, first by number of solved problems (higher first),
-        second by penalty (lower first) and finally, in case of tie, users with the lowest
-        greater accepted time comes first, in case of tie, second lowest greater accepted
-        time come first, and so on.
-        """
-        acc_deltas = sorted(
-            [problem_result.delta() for problem_result in ir.problem_results], key=lambda row: -row
-        )
-        return -ir.solved, ir.penalty, acc_deltas
-
-    instance_results.sort(key=instance_key)
-
-    # set ranks
-    for i in range(1, len(instance_results)):
-        if instance_key(instance_results[i]) == instance_key(instance_results[i - 1]):
-            instance_results[i].rank = instance_results[i - 1].rank
-        else:
-            instance_results[i].rank = instance_results[i - 1].rank + 1
-
-    # find first accepted submission by problem and in the
-    # contest in general.
-    global_instance, global_min_delta = None, float('inf')
-    for i in range(len(problems)):
-        local_instance, local_min_delta = None, float('inf')
-        for ir in instance_results:
-            delta = ir.problem_results[i].delta()
-            if delta < local_min_delta:
-                local_instance, local_min_delta = ir.problem_results[i], delta
-                if delta < global_min_delta:
-                    global_instance, global_min_delta = ir.problem_results[i], delta
-        if local_instance:
-            local_instance.first = True
-
-    if global_instance:
-        global_instance.first_all = True
-
-    return problems, instance_results
 
 
 def fix_problem_folder(problem):
