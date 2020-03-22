ee0c0096df08fe0af0835e56529c50a07ed3f59e
Leandro Castillo Valdes <leandro.castillo.valdes@gmail.com>
2020-03-21 17:29:09 -0700

    [cleanup] Remove unused slack app
    This app was integrated with Slack to report stats, however we are not
    using it anymore and it was a security risk since we didn't check for
    permissions on incoming requests.
    
    In the future, we could add a more secure way to generate stats
    deliverable to any end (e.g emails, slack, discord, messages, etc).

--- slack/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'slack'
-
-urlpatterns = [
-    url(r'^statistics/$', views.statistics, name='statistics'),
-    url(r'^standing/$', views.standing, name='standing'),
-]
