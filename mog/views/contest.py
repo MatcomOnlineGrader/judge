4ed8695060683c5e167bfe70737e0081b0aa4c37
FrankSiret <frank.siret@peoplewalking.com>
2023-01-21 18:42:56 -0500

    register user/team list is loaded asyncs and have more search criteria - is possible to register more than one on user/team in the same request

--- mog/views/contest.py
+++ mog/views/contest.py
@@ -555,6 +555,40 @@ def register_instance(request, contest, user, team):
     return redirect(nxt or reverse('mog:contest_problems', args=(contest.pk, )))
 
 
+@login_required
+def register_multiple_users(request, contest, user_list):
+    nxt = request.POST.get('next')
+    for user in user_list:
+        ContestInstance.objects.create(
+            contest=contest,
+            user=user,
+            real=True,
+            group=contest.group
+        )
+    msg = _('Successfully registered ' + str(len(user_list)) + ' new user')
+    messages.success(request, msg, extra_tags='success')
+    return redirect(nxt or reverse('mog:contest_registration', args=(contest.pk, )))
+
+
+@login_required
+def register_multiple_teams(request, contest, team_list):
+    nxt = request.POST.get('next')
+    for team in team_list:
+        # Check that the team can be registered in the contest
+        if not contest.allow_teams:
+            messages.warning(request, _("The contest doesn't allow teams"), extra_tags='warning')
+            return redirect(nxt or reverse('mog:contests'))
+        ContestInstance.objects.create(
+            contest=contest,
+            team=team,
+            real=True,
+            group=contest.group
+        )
+    msg = _('Successfully registered ' + str(len(team_list)) + ' new team')
+    messages.success(request, msg, extra_tags='success')
+    return redirect(nxt or reverse('mog:contest_registration', args=(contest.pk, )))
+
+
 @public_actions_required
 @login_required
 @require_http_methods(["POST"])
@@ -599,6 +633,7 @@ def contest_register_multiple_users(request, contest_id):
             users.append(get_object_or_404(User, pk=int(member)))
         users = set(users)
         contest = get_object_or_404(Contest, pk=contest_id)
+<<<<<<< HEAD
         for user in users:
             ContestInstance.objects.create(
                 contest=contest,
@@ -613,6 +648,12 @@ def contest_register_multiple_users(request, contest_id):
         messages.error(request, 'Register users: Invalid data!', extra_tags='danger')
 
     return redirect(reverse('mog:contest_registration', args=(contest.pk, )))
+=======
+    except (ValueError, TypeError):
+        messages.error(request, 'Register users: Invalid data!', extra_tags='danger')
+
+    return register_multiple_users(request, contest, users)
+>>>>>>> f2219bb2 (register user/team list is loaded asyncs and have more search criteria)
 
 
 @login_required
@@ -639,12 +680,17 @@ def contest_register_multiple_teams(request, contest_id):
     
     members = request.POST.get('team-members', '').split(',')
     teams = []
+<<<<<<< HEAD
+=======
+    print(members)
+>>>>>>> f2219bb2 (register user/team list is loaded asyncs and have more search criteria)
 
     try:
         for member in members:
             teams.append(get_object_or_404(Team, pk=int(member)))
         teams = set(teams)
         contest = get_object_or_404(Contest, pk=contest_id)
+<<<<<<< HEAD
         for team in teams:
             # Check that the team can be registered in the contest
             if not contest.allow_teams:
@@ -663,6 +709,12 @@ def contest_register_multiple_teams(request, contest_id):
         messages.error(request, 'Register teams: Invalid data!', extra_tags='danger')
 
     return redirect(reverse('mog:contest_registration', args=(contest.pk, )))
+=======
+    except (ValueError, TypeError):
+        messages.error(request, 'Register teams: Invalid data!', extra_tags='danger')
+
+    return register_multiple_teams(request, contest, teams)
+>>>>>>> f2219bb2 (register user/team list is loaded asyncs and have more search criteria)
 
 
 def remove_instance(request, instance):
