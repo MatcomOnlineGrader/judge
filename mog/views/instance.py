cc3075ea1673a1c505744a9e2ba22567d608b0e5
FrankSiret <frank.siret@email.com>
2023-02-13 05:01:46 -0500

    edit team - fix base css - add button to edit description and institution of a team - add bulk edit group - add bulk unregister - add method to get institution by name - fix edit group avoid some invalid group - add number of submissions that every user/team have

--- mog/views/instance.py
+++ mog/views/instance.py
@@ -1,11 +1,14 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.http import Http404, JsonResponse
 from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
-from api.models import ContestInstance
+from api.models import ContestInstance, Institution
 from mog.gating import user_is_admin
 
 
@@ -66,3 +69,38 @@ def instance_edit_render_description(request, instance_pk):
     instance.save()
 
     return redirect('mog:contest_registration', contest_id=instance.contest_id)
+
+
+@login_required
+@require_http_methods(["POST"])
+def instance_edit_team(request, instance_pk):
+    """Set description and institution of team"""
+    if not user_is_admin(request.user):
+        raise Http404()
+
+    instance = get_object_or_404(ContestInstance, pk=instance_pk)
+
+    nxt = request.POST.get('nxt', '') or reverse('mog:contest_registration', args=(instance.contest.pk, ))
+    description = request.POST.get('description', '')
+    institution_id = request.POST.get('institution', '')
+
+    try:
+        institution = Institution.objects.filter(id=int(institution_id)).first()
+        if not institution:
+            raise Exception('Institution with id=%s not exist' % institution_id)
+
+        if instance.team.institution_id != institution.id:
+            instance.team.institution = institution
+
+        if instance.team.description != description:
+            instance.team.description = description
+
+        instance.team.save()
+
+        msg = _("Successfully edited Team '%s'" % instance.team.name)
+        messages.success(request, msg, extra_tags='success')
+
+    except (ValueError, TypeError):
+        messages.error(request, 'Edit team: Invalid data!', extra_tags='danger')
+
+    return redirect(nxt)
