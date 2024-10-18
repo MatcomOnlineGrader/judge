af15451b73198327b0ad0f42891793334b689aad
Karel <karelantoniogz@gmail.com>
2024-10-18 15:32:23 -0400

    Added a script to build and install ochko/safeexec: ci/make_safeexec.sh Modified the dockerfile: install the dependencies and call make_safeexec.sh

---
+++ ci/make_safeexec.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Clone and install the safeexec binary, with some patches
+# Requires: git, cmake, make, g++
+
+info(){
+	echo "[*]" $*
+}
+
+warn(){
+	echo -e "\n[=====================]\n"
+	echo "[!]" $*
+}
+
+build() {
+	# First go to a temp folder
+	info "Changing to a temprorary directory"
+	ODIR=$PWD
+	TDIR=$(mktemp -d)
+	cd $TDIR
+
+	# Now clone and apply the patches
+	info "Cloning safeexec..."
+	git clone https://github.com/ochko/safeexec || { warn "Error cloning repository, aborting"; exit 1; }
+	cd safeexec
+
+	# Run cmake
+	info "Running cmake..."
+	cmake . || { warn "Could not execute cmake, aborting"; exit 1; }
+
+	# Install system-wide
+	info "Installing..."
+	make install || { warn "Could not run `make install`, aborting"; exit 1; }
+
+	# Remove unnecesary stuff in this directory
+	info "Removing unnecesary files..."
+	cd $ODIR
+	rm -rf $TDIR
+
+	info "Everything OK"
+}
+
+build $@
--- dockerfile
+++ dockerfile
@@ -17,6 +17,10 @@ RUN pip install -r requirements.txt
 RUN mkdir -p /var/www/judge/static
 RUN mkdir -p /var/www/judge/media
 
+# Install safeexec
+RUN apt-get install cmake make git g++
+RUN bash ci/make_safeexec.sh && safeexec --exec /bin/true
+
 # Move all static files (js, images, css, etc.) into
 # `/var/www/judge/static` folder. This folder path
 # must match with `STATIC_ROOT` in the `settings.ini`
