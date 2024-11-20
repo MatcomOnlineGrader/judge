4cbe52bfe10bf54aaeb2400ec9f00e9aed0ba2a5
Karel <karelantoniogz@gmail.com>
2024-11-20 12:34:18 -0500

    Refactorings, added logs and minor changes Added some optional params to get_exitcode_stdout_stderr Added some logs Added log setup in the Command (grader.py) Refactorings Minor changes

--- api/management/commands/__utils.py
+++ api/management/commands/__utils.py
@@ -2,10 +2,10 @@ import shlex
 import subprocess
 
 
-def get_exitcode_stdout_stderr(cmd, cwd, env=None):
+def get_exitcode_stdout_stderr(cmd, cwd, env=None, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
     args = shlex.split(cmd)
     proc = subprocess.Popen(
-        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env
+        args, stdin=stdin, stdout=stdout, stderr=stderr, cwd=cwd, env=env
     )
     out, err = proc.communicate()
     exitcode = proc.returncode
