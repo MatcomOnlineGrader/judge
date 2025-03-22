3871781defdd5599c8ddf9d84997ac395bdc3284
Leandro Castillo Valdes <sulkybay@gmail.com>
2025-03-22 15:51:17 -0700

    [WIP] split docker config into dev and prod
    We mount the repository as its own volume to
    simplify development. In production, we serve
    with gunicorn and nginx. In development, we use
    the built-in dev server.
    
    This PR separates the docker config into dev and
    prod for better organization and customization.

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ module.exports = {
   entry: "./frontend/src/index.tsx",
   output: {
     filename: "bundle.js",
-    path: __dirname + "/frontend/static/frontend/dist"
+    path: __dirname + "/frontend/static/frontend/dist",
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: "inline-source-map",
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
 
   module: {
@@ -18,16 +18,16 @@ module.exports = {
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
+        loader: "awesome-typescript-loader",
       },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: "pre",
         test: /\.js$/,
-        loader: "source-map-loader"
-      }
-    ]
+        loader: "source-map-loader",
+      },
+    ],
   },
 
   // When importing a module whose path matches one of the following, just
@@ -36,6 +36,6 @@ module.exports = {
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
     react: "React",
-    "react-dom": "ReactDOM"
-  }
+    "react-dom": "ReactDOM",
+  },
 };
