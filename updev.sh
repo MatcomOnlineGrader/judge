86df010bd827cc959c9ee8f316b4629e088dc468
Leandro Castillo <sulkybay@gmail.com>
2025-09-13 12:24:40 -0700

    create settings.ini placeholder (#164)
    This lets developers know what is expected from
    a configuration perspective. It centralizes the
    settings.ini for development and testing.

--- updev.sh
+++ updev.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-DOCKER_BUILDKIT=0 docker-compose -f docker/dev/docker-compose.yml up
+if [ ! -f "settings.ini" ]; then
+  perl -pe 's/secret_auto_fillable_placeholder/substr("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",rand()*62,12)/ge' settings.ini.template > settings.ini
+fi
+
+database_pass=$(awk -F ":" '/DATABASE_PASS/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' settings.ini)
+
+DOCKER_BUILDKIT=0 DATABASE_PASS=$database_pass \
+    docker-compose -f docker/dev/docker-compose.yml up
